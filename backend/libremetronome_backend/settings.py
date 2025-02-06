682b336701aa72dac014cea002bf9bf6462e098b
hybridpicker <lukas@schoensgibl.com>
2025-02-06 08:58:07 +0100

    setting up for deploy

--- backend/libremetronome_backend/settings.py
+++ backend/libremetronome_backend/settings.py
@@ -12,10 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -79,7 +76,7 @@ WSGI_APPLICATION = 'libremetronome_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
