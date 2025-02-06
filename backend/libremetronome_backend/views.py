73fddfc1c386915e758df1dbd964eb32e453a494
hybridpicker <lukas@schoensgibl.com>
2025-02-06 22:10:30 +0100

    debug settings for react

--- backend/libremetronome_backend/views.py
+++ backend/libremetronome_backend/views.py
@@ -1,5 +1,15 @@
-from django.http import HttpResponseRedirect
+from django.conf import settings
+from django.http import HttpResponseRedirect, Http404
 
 def dev_redirect(request):
-    """Redirect to the React development server."""
-    return HttpResponseRedirect("http://localhost:3000")
+    """
+    Redirects to the React development server only if DEBUG is True.
+    
+    In production (DEBUG=False), this endpoint is disabled and returns a 404 response.
+    """
+    if settings.DEBUG:
+        # In development, redirect to the React dev server.
+        return HttpResponseRedirect("http://localhost:3000")
+    else:
+        # In production, the dev redirect is not available.
+        raise Http404("This endpoint is not available in production.")
