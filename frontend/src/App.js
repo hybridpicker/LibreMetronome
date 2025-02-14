aa561afd129992adca1c8d90af7d74f8cb93978d
hybridpicker <lukas@schoensgibl.com>
2025-02-14 12:49:49 +0100

    Changed license to GPL v3 for LibreMetronome

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,25 @@
 // src/App.js
+
+/*
+ * LibreMetronome - Open Source Metronome
+ *
+ * This file is part of LibreMetronome.
+ *
+ * LibreMetronome is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LibreMetronome is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
 import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
