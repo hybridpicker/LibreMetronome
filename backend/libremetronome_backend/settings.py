8110fd1bacea5967bdd4c99bff503b3d48ec47d3
hybridpicker <lukas@schoensgibl.com>
2025-03-16 11:05:50 +0100

    prepare for 0.5.1

--- backend/libremetronome_backend/settings.py
+++ backend/libremetronome_backend/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
-VERSION = "0.4.8"
+VERSION = "0.5.1"
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
