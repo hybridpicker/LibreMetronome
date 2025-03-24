fd3da2df6e620983ca73b7eece4b08d4fa9a8f95
hybridpicker <lukas@schoensgibl.com>
2025-03-24 06:58:09 +0100

    added support section

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,75 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Mock Web Audio API for tests
+class MockAudioContext {
+  constructor() {
+    this.state = 'running';
+    this.sampleRate = 44100;
+    this.currentTime = 0;
+  }
+
+  createGain() {
+    return {
+      connect: jest.fn(),
+      gain: { value: 1, setValueAtTime: jest.fn() }
+    };
+  }
+
+  createOscillator() {
+    return {
+      connect: jest.fn(),
+      start: jest.fn(),
+      stop: jest.fn(),
+      frequency: { value: 440 }
+    };
+  }
+
+  createBufferSource() {
+    return {
+      connect: jest.fn(),
+      start: jest.fn(),
+      stop: jest.fn()
+    };
+  }
+
+  createBuffer() {
+    return {
+      duration: 1,
+      numberOfChannels: 2,
+      sampleRate: 44100
+    };
+  }
+
+  decodeAudioData() {
+    return Promise.resolve({
+      duration: 1,
+      numberOfChannels: 2,
+      sampleRate: 44100
+    });
+  }
+
+  resume() {
+    this.state = 'running';
+    return Promise.resolve();
+  }
+
+  suspend() {
+    this.state = 'suspended';
+    return Promise.resolve();
+  }
+
+  close() {
+    this.state = 'closed';
+    return Promise.resolve();
+  }
+}
+
+// Add AudioContext to global scope for tests
+global.AudioContext = MockAudioContext;
+global.webkitAudioContext = MockAudioContext;
+
+// Mock requestAnimationFrame
+global.requestAnimationFrame = (callback) => setTimeout(callback, 0);
+global.cancelAnimationFrame = (id) => clearTimeout(id);
