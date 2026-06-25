7ab6de364e881343c832bcab8eb960927008a109
hybridpicker <lukas@schoensgibl.com>
2026-06-25 07:03:16 +0200

    Update devel host and frontend template

--- backend/libremetronome_backend/settings.py
+++ backend/libremetronome_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'SECRET'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['libremetronome.com', 'www.libremetronome.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['libremetronome.com', 'www.libremetronome.com', 'devel.libremetronome.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
